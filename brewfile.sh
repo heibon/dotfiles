b969bc605d472695966ae0caef466e819cbeef7d
Ryosuke Kawabata <ryosuke-kawabata@gmotech.jp>
2016-01-12 12:16:54 +0900

    キーバインドの一部修正とファイル整理

--- brewfile.sh
+++ brewfile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh#latest
 
 # install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ruby -e '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)'
 
 brew update
 
